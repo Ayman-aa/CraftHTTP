729705678357fe6cee92a8c876f83153f938cc4f
frost <romaissaeabde@gmail.com>
2024-11-19 21:46:33 +0100

    Parsing HTTP request

--- httpd/httpd.c
+++ httpd/httpd.c
@@ -8,10 +8,17 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
+#define LISTENADDR "0.0.0.0"
+
+/* Structures */
+struct sHttpRequest {
+  char method[8];
+  char url[128];
+};
+typedef struct sHttpRequest httpreq;
+
 /* GLOBAL */
 char *error;
-
-#define LISTENADDR "0.0.0.0"
 
 /* return -1 if there is an error, otherwise the socket fd */
 int srv_init(int port)
@@ -28,7 +35,6 @@ int srv_init(int port)
 
   srv.sin_family = AF_INET;
   srv.sin_addr.s_addr = inet_addr(LISTENADDR);
-  printf("%f\n", inet_addr(LISTENADDR));
   srv.sin_port = htons(port);
 
   if (bind(returnValue, (struct sockaddr *)&srv, sizeof(srv)))
@@ -65,8 +71,71 @@ int cli_accept(int s)
   return c;
 }
 
+/* returns 0 on error, otherwize it returns struct sHttpRequest */
+httpreq *parse_http(char *str)
+{
+  httpreq *req;
+  char *p;
+
+  req = malloc(sizeof(struct sHttpRequest));
+  if (!req) return NULL;
+  for (p = str; *p && *p != ' '; p++);
+  if (*p == ' ')
+    *p = 0;
+  else {
+    error = "parse_http() NOSPACE error";
+    free(req);
+    return 0;
+  }
+  strncpy(req->method, str, 7);
+
+  for (str=++p; *p && *p != ' '; p++);
+  if (*p == ' ')
+    *p = 0;
+  else {
+      error = "parse_http() 2N_NOSPACE error";
+      free(req);
+      return 0;
+  }
+  strncpy(req->url, str, 127);
+  return req;
+}
+
+/* returns 0 in error, otherwize return data */
+char *cli_read(int c)
+{
+    static char buf[512];
+
+    memset(&buf, 0, 512);
+    if (read(c, buf, 511) < 0)
+    {
+        error = "read() error";
+        return 0;
+    }
+    else
+        return buf;
+}
+
 void cli_conn(int s, int c)
 {
+    httpreq *req;
+    char *p;
+
+    p = cli_read(c);
+    if (!p)
+    {
+        fprintf(stderr, "%s\n", error);
+        close(c);
+        return ;
+    }
+    req = parse_http(p);
+    if (!req)
+    {
+        fprintf(stderr, "%s\n", error);
+        close(c);
+        return ;
+    }
+    printf("method: '%s'\nUrl: '%s'\n", req->method, req->url);
     return ;
 }
 
@@ -101,7 +170,6 @@ uint64_t main ( int ac, char *av[])
     printf("Incomming connection\n");
     if (!fork())
       cli_conn(s, c);
-    close(c);
   }
   return -1;
 }
---
+++ segFault.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int ac, char *av[])
+{
+    (void)ac;
+    (void)av;
+
+    printf("method: '%s'\nUrl: '%s'\n");
+}
