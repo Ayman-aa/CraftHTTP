01283028b5b6e6285d833b0a7f8a100a94ea40f6
aaamam <aaamam@e1r12p9.1337.ma>
2024-12-05 02:51:02 +0100

    ghashwia

---
+++ includes/Location.hpp
@@ -0,0 +1,12 @@
+#ifndef LOCATION_HPP
+#define LOCATION_HPP
+
+#include "ServerConfiguration.hpp"
+class Location
+{
+    public:
+        bool autoindex;
+        vector<string> methods;
+};
+
+#endif
