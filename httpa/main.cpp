43af940b4b0345298e5eab5f60a6faeb2d4d2bce
48k483x <romaissaeabde@gmail.com>
2025-01-07 20:47:12 +0100

    [ Edit Makefile, just styling stuff... ]

--- httpa/main.cpp
+++ httpa/main.cpp
@@ -133,57 +133,6 @@ void printAllServers(const vector<ServerConfiguration*>& servers) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*int main(int ac, char *av[])
-{
-	if (ac != 2) {
-		if (ac > 2) {
-			fprintf(stderr, "[Error] Invalid number of arguments. Usage: %s <config_file>\n", av[0]);
-			return 1;
-		}
-		fprintf(stderr, "[Warning] No configuration file provided. Proceeding with default configuration.\n");
-		filePath = "conf/test.yaml";
-	}
-	else
-		filePath = av[1];
-	fprintf(stdout, "filePath: %s\n", filePath);
-	string fp = filePath;
-
-	try {
-		ConfigurationParser fileParser(fp);
-		printAllServers(fileParser.servers);
-		//printAllLocations(fileParser);
-	}
-	catch (exception& e) {
-		cout << e.what() << endl;
-	}
-	return 0;
-}
-*/
-
 int main(int ac, char *av[]) {
     std::string filePath;
 
