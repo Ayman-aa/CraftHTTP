f7b38cd361be74a7987fbd31ee7baaf4a3068dbe
48k483x <romaissaeabde@gmail.com>
2025-01-13 14:48:09 +0100

    [ ✔️ ]

---
+++ httpa/includes/cluster.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#ifndef CLUSTER_HPP
+# define CLUSTER_HPP
+
+class cluster {
+	public:
+		cluster(Configuration& config);
+		~cluster();
+		void run();
+
+	private:
+		int epoll_fd;
+		Configuration config;
+		Vector<Server*> servers;
+		map<int, int> client_to_server;
+		map(int, Server*) server_fd_to_server;
+
+		void createEpoll();
+		void addSocketToEpoll(int fd);
+		void handleEvents(struct epoll_event* events, int numEvents);
+		void acceptConnections(int serverSocket);
+		void handleClient(int client_fd, uint32_t events);
+		void serverErrorPage(int client_fd, int error_page, Server* server);
+		bool isServerFd(int fd);
+		void cleanup();
+};
+
+#endif
