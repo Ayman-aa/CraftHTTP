b3a59e53b90bc43188e44823719537445847ec11
48k483x <romaissaeabde@gmail.com>
2025-01-13 14:51:19 +0100

    [ ✔️ ]

--- httpa/includes/configParser.hpp
+++ httpa/includes/configParser.hpp
@@ -28,51 +28,51 @@ class ConfigurationParser : public ServerConfiguration
 		}
 
 		/* --------------- Main Config Data ----------------- */
-		vector<ServerConfiguration*> 	servers;
-		ServerConfiguration 			currentServer;
+		vector<ServerConfiguration*>	servers;
+		ServerConfiguration				currentServer;
 
 		/* --------------- Parsing Functions ----------------- */
-		void load(ifstream& file);
-		int getIndentLevel(const string& line);
-		bool isValidRootLevel(string& line);
-		bool verifyLineFormat(string& line, int indentLevel);
-		bool isValidSecondLevel(string& line);
+		void	load(ifstream& file);
+		int		getIndentLevel(const string& line);
+		bool	isValidRootLevel(string& line);
+		bool	verifyLineFormat(string& line, int indentLevel);
+		bool	isValidSecondLevel(string& line);
 		
 		/* --------------- Utility Functions ----------------------- */
-		void syntaxError(int currentLineNumber, const string& errorMessage);
-		void clear_kv(key_value& kv);
-		bool LineIsCommentOrEmpty(string& line);
+		void	syntaxError(int currentLineNumber, const string& errorMessage);
+		void	clear_kv(key_value& kv);
+		bool	LineIsCommentOrEmpty(string& line);
 
-        /* --------------- Host & Port Processing ----------------- */
-		bool extractHostKey(key_value& k_v);
-		bool isValidIPSegment(const string& segment);
-		bool extractPortValue(key_value& k_v);
-		bool isValidPortSegment(const string& segment);
+		/* --------------- Host & Port Processing ----------------- */
+		bool	extractHostKey(key_value& k_v);
+		bool	isValidIPSegment(const string& segment);
+		bool	extractPortValue(key_value& k_v);
+		bool	isValidPortSegment(const string& segment);
 	
-        /* --------------- Server Configuration ------------------- */
-		bool extractServerNamesValue(key_value& k_v);
-		bool extractClientMaxBodySizeValue(key_value& k_v);
-		bool extractErrorPages(ifstream& file, int& currentLineNumber);
+		/* --------------- Server Configuration ------------------- */
+		bool	extractServerNamesValue(key_value& k_v);
+		bool	extractClientMaxBodySizeValue(key_value& k_v);
+		bool	extractErrorPages(ifstream& file, int& currentLineNumber);
 
-        /* --------------- Location Configuration ---------------- */
-		bool extractLocationInfos(ifstream& file, int& currentLineNumber, Location& location);
-		bool servLocationLine(key_value& k_v, Location& location);
-		bool extractAutoIndexValue(key_value& k_v, Location& location);
-		bool extractAllowedMethods(key_value& k_v, Location& location);
-		bool isValidMethod(const string& method);
-		bool extractIndexValues(key_value& k_v, Location& location);
-		bool isValidIndex(const string& index);
-		bool extractReturnValue(key_value& k_v, Location& location);
-		bool extractRootValue(key_value& k_v, Location& location);
+		/* --------------- Location Configuration ---------------- */
+		bool	extractLocationInfos(ifstream& file, int& currentLineNumber, Location& location);
+		bool	servLocationLine(key_value& k_v, Location& location);
+		bool	extractAutoIndexValue(key_value& k_v, Location& location);
+		bool	extractAllowedMethods(key_value& k_v, Location& location);
+		bool	isValidMethod(const string& method);
+		bool	extractIndexValues(key_value& k_v, Location& location);
+		bool	isValidIndex(const string& index);
+		bool	extractReturnValue(key_value& k_v, Location& location);
+		bool	extractRootValue(key_value& k_v, Location& location);
 
-        /* --------------- CGI Configuration -------------------- */
-		bool isValidCgiKey(const string& method);
-		bool extractCgiPath(ifstream& file, Location& location, int& currentLineNumber);
-		bool extractUploadPath(key_value& k_v, Location& location);
+		/* --------------- CGI Configuration -------------------- */
+		bool	isValidCgiKey(const string& method);
+		bool	extractCgiPath(ifstream& file, Location& location, int& currentLineNumber);
+		bool	extractUploadPath(key_value& k_v, Location& location);
 
 		/* ---------- Helper method, bash nrje3 offset ---------- */
-		bool FileSeekg(ifstream& file, const string& line, int &currentLineNumber, bool retVal);
+		bool	FileSeekg(ifstream& file, const string& line, int &currentLineNumber, bool retVal);
 };
-typedef ConfigurationParser Configurations;
+		typedef ConfigurationParser Configurations;
 
 #endif /* ayeh ayeh, configParser.hpp */
